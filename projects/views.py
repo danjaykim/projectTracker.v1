2f816a849a2c4cd5d1e3c1b9dbee51d5a57325f8
Daniel Kim <danjkim831@gmail.com>
2024-05-28 13:56:37 -0700

    Feature 8 complete

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from projects.models import Project
 
 # Create your views here.
 
 
 # views for all instances of the Project model:
+@login_required
 def list_projects(request):
-    project_list = Project.objects.all()
+    project_list = Project.objects.filter(owner=request.user)
 
     context = {
         "project_list": project_list,
