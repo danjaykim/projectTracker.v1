9923935619f8e878df97e9ac8876e0d900424668
Daniel Kim <danjkim831@gmail.com>
2024-05-31 15:09:15 -0700

    Added media queries and updated notes detail page

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from notes.views import list_notes, note_detail, note_create
+from notes.views import list_notes, note_detail, note_create, note_edit
 
 
 urlpatterns = [
     path("", list_notes, name="list_notes"),
     path("<int:id>/", note_detail, name="note_detail"),
+    path("<int:id>/edit/", note_edit, name="note_edit"),
     path("create/", note_create, name="note_create"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from notes.models import Note
 from notes.forms import NoteForm
+import random
 
 
 # Create your views here.
@@ -20,9 +21,12 @@ def list_notes(request):
 @login_required
 def note_detail(request, id):
     note = get_object_or_404(Note, id=id)
+    colors = ["#FFF68F", "#73D949", "#F38FB7", "#62C9F6"]
+    random_color = random.choice(colors)
 
     context = {
         "note": note,
+        "background_color": random_color,
     }
 
     return render(request, "notes/note_detail.html", context)
@@ -47,3 +51,26 @@ def note_create(request):
     }
 
     return render(request, "notes/note_create.html", context)
+
+
+# view for editing a particular note:
+@login_required
+def note_edit(request, id):
+    n_edit = get_object_or_404(Note, id=id)
+
+    if request.method == "POST":
+        form = NoteForm(request.POST, instance=n_edit)
+        if form.is_valid():
+            edited_note = form.save(commit=False)
+            edited_note.owner = request.user
+            edited_note.save()
+            return redirect("note_detail", id=n_edit.id)
+
+    elif request.method == "GET":
+        form = NoteForm(instance=n_edit)
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "notes/note_edit.html", context)
